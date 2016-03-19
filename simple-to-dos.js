10652227dd175d90727edc587400d6dfc789613f
Jacinda <jacindaz@gmail.com>
2016-03-19 12:12:02 -0400

    Add ability to submit form, to create new task
     - JS allows us to submit the form
     - adds event listener
     - there is no "submit" button, it submits form after
       you press enter

--- simple-to-dos.js
+++ simple-to-dos.js
@@ -1,9 +1,29 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1"},
-      { text: "This is task 2"},
-      { text: "This is task 3"}
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // prevent default browser form submit
+      event.preventDefault();
+
+      // get value from form element
+      // this is what I type in the text box
+      var text = event.target.text.value;
+
+      // insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+    }
   });
 }
